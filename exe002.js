dc8a168c56f18f249e24ef05633a71c2f9c022f4
Guilherme Oliveira <oliveiragl615@gmail.com>
2023-03-07 12:50:47 -0300

    Update exe002.js

---
+++ exe002.js
@@ -0,0 +1,12 @@
+/*
+    There are three things wrong with this code.
+    Find them and explain why they are wrong.
+*/
+
+// that variable has invalid name, shoud be 'someNumber', not 'some Numeber'. There is an extra space. And we should be using 'let' here because later in the code, we re-assign the value of the variable.
+
+const some Number = 20;
+
+// and that variable call are invalid, because de name of the variable doens't exist, and don't have a semicolon on the final of the expression.
+
+someNumber = 50
