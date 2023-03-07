856f86c3ef7892fbc1d4d7f7a5d062408d1cee94
Guilherme Oliveira <oliveiragl615@gmail.com>
2023-03-07 13:37:05 -0300

    Create exe004.js

---
+++ exe004.js
@@ -0,0 +1,13 @@
+/*
+    Why is this code invalid? Edit this until it is valid.
+*/
+
+// to declare object properties, we use commas between properties and not semicolons.
+
+const objectVariable = {
+    property1: 'i am property 1',
+    property2: 'i am property 2',
+    property3: [20, 30, 40]
+};
+
+console.log(objectVariable.property3[2])
